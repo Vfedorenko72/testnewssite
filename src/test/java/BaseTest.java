e0bbd2bf224c99b7ad368e21cf502e23dc8860fc
Vfedorenko72 <v.fedorenko@sh.dunice.net>
2024-11-22 17:59:57 +0300

    добавил NewsSiteTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,33 @@
-public class BaseTest {
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
+import static com.codeborne.selenide.Selenide.$x;
+
+abstract class BaseTest {
+    private final static String BECK_URL = "https://api.news.academy.dunice.net/";
+    private final static String APP_URL = "https://news.academy.dunice.net/start";
+
+    public static void setUp() {
+        Configuration.browser = "chrome";
+    }
+    @BeforeAll
+    public static void init() {
+        setUp();
+        entrance();
+    }
+    @AfterAll
+    public static void tearDown() {
+
+    }
+
+    /**
+     * Вход на главную страницу
+     */
+    public static void entrance() {
+        Selenide.open(APP_URL);
+        $x("//input[@id=\":r0:\"]").setValue(BECK_URL);
+        $x("//button").click();
+    }
 }
